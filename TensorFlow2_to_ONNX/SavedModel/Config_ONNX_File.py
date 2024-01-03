b4b55ea7b24c42fa48cdf76146e19bd315245fcd
PaolaRoC <140692213+PaolaRoC@users.noreply.github.com>
2024-01-03 15:17:58 +0100

    Add files via upload

---
+++ TensorFlow2_to_ONNX/SavedModel/Config_ONNX_File.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 11:11:33 2023
+
+@author: Paola
+"""
+import onnx
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+     '--load_tempmodel', help='load the temporary ONNX model.', required=True)
+    parser.add_argument(
+    '--save_onnxmodel', help='Save the ONNX model with explicit batch', required=True)
+    args = parser.parse_args()
+    
+
+    BATCH_SIZE =32
+    onnx_model = onnx.load_model(args.load_tempmodel)
+    inputs = onnx_model.graph.input
+    for input in inputs:
+        dim1 = input.type.tensor_type.shape.dim[0]
+        dim1.dim_value = BATCH_SIZE
+    
+    model_name = args.save_onnxmodel
+    onnx.save_model(onnx_model, model_name)
+    print("Done saving!")
+
+
+if __name__ == '__main__':
+  main()
